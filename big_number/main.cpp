ed09d4fdc5c940e85fd7f48ef438e877fd556fec
李晓洋 <lixiaoyang@ebupt.com>
2023-04-03 23:30:00 +0800

    add big integer

--- big_number/main.cpp
+++ big_number/main.cpp
@@ -45,6 +45,36 @@ vector<int> sub(vector<int> &A, vector<int> &B)
     return C;
 }
 
+vector<int> mul(vector<int> &A, int B)
+{
+    vector<int> C;
+    
+    long t = 0;
+    for (int i = 0; i < A.size() || t; i++)
+    {
+        if(i < A.size())t += A[i] * B;
+        C.push_back(t % 10);
+        t /= 10;
+    }
+    
+    return C;
+}
+
+vector<int> div(vector<int> &A, int &B, int &r)
+{
+    vector<int> C;
+    r = 0;
+    for (int i = A.size() - 1; i >= 0; i--)
+    {
+        r = r * 10 + A[i];
+        C.push_back(r / B);
+        r %= B;
+    }
+    reverse(C.begin(),C.end());
+    while (C.size() > 1 && C.back() == 0) C.pop_back();
+    return C;
+}
+
 
 int main()
 {
